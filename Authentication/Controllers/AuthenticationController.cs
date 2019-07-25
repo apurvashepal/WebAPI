4c7919ac94904c6a4b580a2b77da36de6769672c
apurvashepal <shepalag28@gmail.com>
2019-07-25 22:52:21 +0530

    adding Authentication

---
+++ Authentication/Controllers/AuthenticationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Authentication.Models;
+
+namespace Authentication.Controllers
+{
+    public class AuthenticationController : ApiController
+    {
+		
+		[Authorize]
+		public List<Employee> GetEmployees()
+		{
+			using (var db = new AuthenticationEntities())
+			{
+				List<Employee> employees = db.Employees.ToList();
+				return employees;
+			}
+		}
+	}
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,17 @@
+﻿CREATE DATABASE TEST  
+GO    
+
+USE TEST  
+GO    
+
+CREATE TABLE Users(Id INT IDENTITY(1,1) PRIMARY KEY, Name varchar(255) NOT NULL, UserName varchar(50), Password varchar(50))  
+INSERT INTO [TEST].[dbo].[Users](Name, UserName, Password) VALUES('Mukesh Kumar', 'Mukesh',' AAAAAA');  
+  
+
+CREATE TABLE Employees(Id INT IDENTITY(1,1) PRIMARY KEY, Name varchar(255) NOT NULL, Address varchar(500))  ;
+
+INSERT INTO Employees (Name, Address) VALUES('Mukesh Kumar', 'New Delhi')  
+INSERT INTO Employees (Name, Address) VALUES('John Right', 'England')  
+INSERT INTO Employees (Name, Address) VALUES('Chris Roy', 'France')  
+INSERT INTO Employees (Name, Address) VALUES('Anand Mahajan', 'Canada')  
+INSERT INTO Employees (Name, Address) VALUES('Prince Singh', 'India')  
